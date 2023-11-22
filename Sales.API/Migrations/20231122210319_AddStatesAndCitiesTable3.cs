b9823d1738d65a8d9cc668c8e3ab7bbba7ce610c
JUAN DAVID ZAPATA ROBLEDO <juandavidzr@hotmail.com>
2023-11-22 17:01:37 -0500

    add states number

---
+++ Sales.API/Migrations/20231122210319_AddStatesAndCitiesTable3.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Sales.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddStatesAndCitiesTable3 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Cities_StateId",
+                table: "Cities");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Cities_StateId_Name",
+                table: "Cities",
+                columns: new[] { "StateId", "Name" },
+                unique: true,
+                filter: "[StateId] IS NOT NULL");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Cities_StateId_Name",
+                table: "Cities");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Cities_StateId",
+                table: "Cities",
+                column: "StateId");
+        }
+    }
+}
