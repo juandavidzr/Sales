6a02fcb7be6249b92176f13c942e020d9ad99757
JUAN DAVID ZAPATA ROBLEDO <juandavidzr@hotmail.com>
2023-12-06 11:58:59 -0500

    secutiry hardcoded on web

--- Sales.WEB/Program.cs
+++ Sales.WEB/Program.cs
@@ -3,13 +3,18 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using CurrieTechnologies.Razor.SweetAlert2;
 using Sales.WEB;
+using Microsoft.AspNetCore.Components.Authorization;
+using Sales.WEB.Auth;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("http://localhost:5239/") });
+builder.Services.AddSingleton(sp => new HttpClient { BaseAddress = new Uri("http://localhost:5239/") });
 builder.Services.AddScoped<IRepository, Repository>();
 builder.Services.AddSweetAlert2();
+builder.Services.AddAuthorizationCore();
+builder.Services.AddScoped<AuthenticationStateProvider, AuthenticationProviderTest>();
+
 
 await builder.Build().RunAsync();
