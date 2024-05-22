8070aa7821984ad68e1a10da7ebd0abffef14c71
Juan David <juandavidzr@hotmail.com>
2024-05-22 17:23:54 -0500

    user manager

--- Sales.API/Data/DataContex.cs
+++ Sales.API/Data/DataContex.cs
@@ -11,24 +11,14 @@ namespace Sales.API.Data
         }
         public DbSet<Category> Categories { get; set; }
         public DbSet<City> Cities { get; set; }
-        
         public DbSet<Country> Countries { get; set; }
-
         public DbSet<Product> Products { get; set; }
-
         public DbSet<ProductCategory> ProductCategories { get; set; }
-
         public DbSet<ProductImage> ProductImages { get; set; }
-
         public DbSet<State> States { get; set; }
-
         public DbSet<TemporalSale> TemporalSales { get; set; }
-
         public DbSet<Sale> Sales { get; set; }
-
         public DbSet<SaleDetail> SaleDetails { get; set; }
-
-
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
